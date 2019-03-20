f753c6c6deff5e8f09eb2fc13bcb7e7a25bff5cd
easywaldoji <easywaldoji@gmail.com>
2019-03-20 22:54:21 +0900

    TypeScript 놀이터
    TypeScript 를 손에 익숙하게 하기 위한 놀이터 공간이다.

---
+++ hello.js
@@ -0,0 +1,31 @@
+var isDone = false;
+var MyName = "waldo";
+var age = 39;
+var habits = ["reading books", "coding", "watching movie"];
+var numList = [1, 2, 3, 4, 5];
+var tuple = ["waldo", 100];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Green"] = 2] = "Green";
+    Color[Color["Blue"] = 3] = "Blue";
+})(Color || (Color = {}));
+var c = Color.Blue;
+console.log(c);
+var Car;
+(function (Car) {
+    Car[Car["Avavante"] = 1] = "Avavante";
+    Car[Car["Sonata"] = 2] = "Sonata";
+    Car[Car["Matiz"] = 3] = "Matiz";
+})(Car || (Car = {}));
+;
+var carName = Car[2];
+console.log(carName);
+PrintInfo();
+function PrintInfo() {
+    console.log(isDone);
+    habits.map(function (e) {
+        console.log(e);
+    });
+    console.log(c);
+}
---
+++ hello.ts
@@ -0,0 +1,27 @@
+let isDone:Boolean = false;
+let MyName:String = "waldo";
+let age:Number = 39;
+let habits:Array<String> = ["reading books", "coding", "watching movie"];
+let numList:Number[] = [1,2,3,4,5];
+let tuple:[String, Number] = ["waldo", 100];
+enum Color {Red = 1, Green, Blue}
+let c:Color = Color.Blue;
+
+console.log(c);
+
+enum Car {
+    Avavante = 1,
+    Sonata = 2,
+    Matiz =3
+};
+let carName:string = Car[2];
+console.log(carName);
+
+PrintInfo();
+function PrintInfo() {
+    console.log(isDone);
+    habits.map(e => {
+        console.log(e);
+    });
+    console.log(c);
+}
