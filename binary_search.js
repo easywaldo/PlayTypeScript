b3fc24c9b4937557de41dbaee278624d49dad93b
easywaldoji <easywaldoji@gmail.com>
2019-05-27 00:34:41 +0900

    의존성관리 및 TypeScript 환경설정 추가

---
+++ binary_search.js
@@ -0,0 +1,14 @@
+var points = [1, 3, 5, 7, 9, 10];
+var targetNumber = 6;
+// function SearchBinary(target:Number, source:Number[]) {
+//     let start:Number = 0;
+//     let end:Number = source.length -1;
+//     while(start <= end) {
+//         let middle:any = end / 2;
+//         let searched:Number = source[middle];
+//     }
+// }
+var end = 12 - 1;
+var start = 0;
+var middle = end / 2;
+console.log(middle);
---
+++ binary_search.ts
@@ -0,0 +1,20 @@
+let points:Number[] = [1,3,5,7,9,10];
+
+let targetNumber:Number = 6;
+
+// function SearchBinary(target:Number, source:Number[]) {
+//     let start:Number = 0;
+//     let end:Number = source.length -1;
+
+//     while(start <= end) {
+//         let middle:any = end / 2;
+//         let searched:Number = source[middle];
+//     }
+// }
+
+let end:number = 12 -1;
+let start:Number = 0;
+
+let middle:any = end / 2;
+
+console.log(middle);
