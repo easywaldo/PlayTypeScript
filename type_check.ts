a737ff52cacc440151639405ce83740a61219f47
easywaldoji <easywaldoji@gmail.com>
2021-08-03 00:56:37 +0900

    Type check sample
    동적 값 할당에 의한 변수 선언 테스트

--- type_check.ts
+++ type_check.ts
@@ -1,2 +1,13 @@
 let number = (3).toString() + [1].toString();
 console.log(number);
+
+
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: b,
+}
+let d = c.apple * 4;
+
+console.log(a,b,c,d);
