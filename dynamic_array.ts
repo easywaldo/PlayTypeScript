d636bdc7494047229579be3de768f6c869d0448e
easywaldoji <easywaldoji@gmail.com>
2021-08-31 23:51:52 +0900

    type array and any type array sample

--- dynamic_array.ts
+++ dynamic_array.ts
@@ -11,3 +11,12 @@ myArray.push("easy waldo"); // allowed String
 myArray.push(999); // allowed number
 
 console.log(myArray);
+
+
+let anyArray = [];
+anyArray.push(1);
+anyArray.push("hello");
+
+let numArray :number[] = [];
+numArray.push(1);
+numArray.push("hello");
