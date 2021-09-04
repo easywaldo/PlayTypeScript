0d79a890a07cc540c42fa1f494b0e99d87f04e1b
easywaldoji <easywaldoji@gmail.com>
2021-09-05 01:27:25 +0900

    Optional type param function sample

--- optional_func.ts
+++ optional_func.ts
@@ -5,3 +5,17 @@ function log(message: string, userId?: string): void {
 
 log('Page loaded');
 log('User signed in', 'easywaldo');
+
+
+type Context = {
+    appId?: string
+    userId?: string
+}
+
+function appLog(message: string, context: Context={}): void {
+    let time = new Date().toISOString();
+    console.log(time, message, context.userId);
+}
+
+appLog('app initialized');
+appLog('app launched', {appId: "superb", userId: "easywaldo"});
