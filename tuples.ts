24a973200387cc879ff1d846ab72af86b6ba46d3
easywaldoji <easywaldoji@gmail.com>
2021-09-05 00:41:14 +0900

    rest element tuple sample

--- tuples.ts
+++ tuples.ts
@@ -2,5 +2,13 @@ let student: [string, string, number] = ['computer', 'easywaldo', 20];
 console.log(student);
 
 student = ['math', 'tom', 19];
-student = ['science', 'bob', 'error'];
+//student = ['science', 'bob', 'error']; //error message:
 
+
+let friends: [string, ...string[]] = ['jane', 'kayazu', 'joe', 'leo'];
+
+let mixed: [number, boolean, ...string[]] = [1, false, 'horse', 'dog', 'cat'];
+
+
+console.log(friends);
+console.log(mixed);
