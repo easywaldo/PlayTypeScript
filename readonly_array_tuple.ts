4f06ac4ee4f6e46078eee2791cb13677fd008c8e
easywaldoji <easywaldoji@gmail.com>
2021-09-05 00:48:16 +0900

    Readonly 정의방법 2가지 sample

--- readonly_array_tuple.ts
+++ readonly_array_tuple.ts
@@ -1,5 +1,14 @@
 let as: readonly number[] = [1,2,3];
 let bs: readonly number[] = as.concat(4);
 let three = bs[2];
+
+
+type A = readonly string[];
+type B = ReadonlyArray<string>;
+type C = Readonly<string[]>;
+type D = readonly [number, string];
+type E = Readonly<[number, string]>;
+
+
 as[4] = 5;      //error
 as.push(6);     //error
