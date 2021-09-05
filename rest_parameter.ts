b8b52e1279065bf6a544160801a404ac7e284d6c
easywaldoji <easywaldoji@gmail.com>
2021-09-05 23:31:45 +0900

    가변갯수 파라미터 샘플

--- rest_parameter.ts
+++ rest_parameter.ts
@@ -3,3 +3,11 @@ function sum(numbers: number[]) : number {
 }
 
 console.log(sum([1,2,3]));
+
+
+function sumVariadic(...numbers: number[]): number {
+    // @ts-ignore
+    return Array.from(arguments).reduce((total, v) => total + v , 0);
+}
+
+console.log(sumVariadic(1,2,3,4,5));
