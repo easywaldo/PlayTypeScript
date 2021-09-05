8bf3e81f60d39127220a2272cf76f94616ee751d
easywaldoji <easywaldoji@gmail.com>
2021-09-05 23:56:30 +0900

    call,apply,bind sample

--- rest_parameter.ts
+++ rest_parameter.ts
@@ -11,3 +11,12 @@ function sumVariadic(...numbers: number[]): number {
 }
 
 console.log(sumVariadic(1,2,3,4,5));
+
+function add(a: number, b: number) : number {
+    return a + b;
+}
+
+console.log(add(10, 20));
+console.log(add.apply(null, [10, 20]));
+console.log(add.call(null, 10, 20));
+console.log(add.bind(null, 10, 20)());
